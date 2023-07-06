8a088d25dfa4ea462dbd48dc12f5c0ac6d8cca5b
john-slayer666 <129868049+john-slayer666@users.noreply.github.com>
2023-07-06 15:15:57 +0300

    index.jsx

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -129,7 +129,7 @@ React.useEffect(() => {
           </Button>
           <img
             className={styles.image}
-            src={`http://localhost:4444${imageUrl}`}
+            src={`${process.env.REACT_APP_API_URL}${imageUrl}`}
             alt='Uploaded'
           />
         </>
