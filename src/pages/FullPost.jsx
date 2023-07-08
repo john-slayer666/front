b3330fc2425686796d005c4da04160cbb6d1a4be
john-slayer666 <zambross99999@gmail.com>
2023-07-08 12:00:25 +0300

    return FullPost

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -1,95 +1,20 @@
-// import React from "react";
-// import axios from "../axios";
-// import { Post } from "../components/Post";
-// import { Index } from "../components/AddComment";
-// import { CommentsBlock } from "../components/CommentsBlock";
-// import { useParams } from 'react-router-dom';
-
-// import { set } from "react-hook-form";
-// import { ReactMarkdown } from "react-markdown/lib/react-markdown";
-
- 
-// export const FullPost = () => {
-//   const [data, setData] = React.useState();
-//   const [isLoading, setLoading] = React.useState(true);
-//   const {id} = useParams();
-
-//   React.useEffect(() => {
-//     axios
-//       .get(`/posts/${id}`)
-//       .then((res) => {
-//         setData(res.data);
-//         setLoading(false);
-//       })
-//       .catch((err) => {
-//         console.warn(err);
-//         alert('Ошибка получения статьи');
-        
-//       });
-
-//   }, [id]);
-
-//   if (isLoading) {
-//     return <Post isLoading={isLoading} isFullPost />;
-//   }
-
-
-//   return (
-//     <>
-//       <Post
-//         id={data._id}
-//         title={data.title}
-//         imageUrl={data.imageUrl ?`${process.env.REACT_APP_API_URL}${data.imageUrl}`: ''}
-//         user={data.user}
-//         createdAt={data.createdAt}
-//         viewsCount={data.viewsCount}
-//         commentsCount={3}
-//         tags={data.tags}
-//         isFullPost
-//       >
-//         <ReactMarkdown children={data.text} />
-//       </Post>
-//       <CommentsBlock
-//         items={[
-//           {
-//             user: {
-//               fullName: 'Вася Пупкин',
-//               avatarUrl: 'https://mui.com/static/images/avatar/1.jpg',
-//             },
-//             text: 'Это тестовый комментарий 555555',
-//           },
-//           {
-//             user: {
-//               fullName: 'Иван Иванов',
-//               avatarUrl: 'https://mui.com/static/images/avatar/2.jpg',
-//             },
-//             text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
-//           },
-//         ]}
-//         isLoading={false}
-//       >
-//         <Index />
-//       </CommentsBlock>
-//     </>
-//   );
-// };
-
-
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
+import axios from "../axios";
 import { Post } from "../components/Post";
+import { Index } from "../components/AddComment";
 import { CommentsBlock } from "../components/CommentsBlock";
 import { useParams } from 'react-router-dom';
-import { ReactMarkdown } from "react-markdown";
-import { useForm } from 'react-hook-form';
 
+import { set } from "react-hook-form";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+
+ 
 export const FullPost = () => {
-  const [data, setData] = useState();
-  const [isLoading, setLoading] = useState(true);
-  const { id } = useParams();
-  const { handleSubmit, errors, register } = useForm();
+  const [data, setData] = React.useState();
+  const [isLoading, setLoading] = React.useState(true);
+  const {id} = useParams();
 
-  useEffect(() => {
+  React.useEffect(() => {
     axios
       .get(`/posts/${id}`)
       .then((res) => {
@@ -99,30 +24,28 @@ export const FullPost = () => {
       .catch((err) => {
         console.warn(err);
         alert('Ошибка получения статьи');
+        
       });
+
   }, [id]);
 
   if (isLoading) {
     return <Post isLoading={isLoading} isFullPost />;
   }
 
-  const onSubmit = (formData) => {
-    alert('Post submitted'); // Replace with desired functionality
-  };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <>
       <Post
         id={data._id}
         title={data.title}
-        imageUrl={data.imageUrl ? `${process.env.REACT_APP_API_URL}${data.imageUrl}` : ''}
+        imageUrl={data.imageUrl ?`${process.env.REACT_APP_API_URL}${data.imageUrl}`: ''}
         user={data.user}
         createdAt={data.createdAt}
         viewsCount={data.viewsCount}
         commentsCount={3}
         tags={data.tags}
         isFullPost
-        ref={register({ required: 'Post is empty' })}
       >
         <ReactMarkdown children={data.text} />
       </Post>
@@ -144,7 +67,9 @@ export const FullPost = () => {
           },
         ]}
         isLoading={false}
-      />
-    </form>
+      >
+        <Index />
+      </CommentsBlock>
+    </>
   );
 };
